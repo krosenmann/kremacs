b6b6b7bb2df2262d5b287e7450ba4feae3493879
Roman Zayrullin <krosenmann@gmail.com>
2015-12-29 17:20:31 +0300

    Добавил функцию для вставки заголовков файлов, исправил генерацию партии баяна

--- plugins/lilyfunc.el
+++ plugins/lilyfunc.el
@@ -4,14 +4,23 @@
    ;;Function for autopaste bayan Staffs
    ;; (Right hand, left hand: bass, chords and step voice with mask
    (interactive)
-(insert " 
- \\new Staff  { <<
-    \\new Voice {\\voiceOne \\stemNeutral }
-    \\new Voice {\\voiceTwo \\stemNeutral}
+(insert " \clef bass
+  { <<
+    \\new Voice {\\voiceOne \\stemNeutral \n }
+    \\new Voice {\\voiceTwo \\stemNeutral \n}
     \\new Voice {\\voiceThree \\stemNeutral
 		\\override ParenthesesItem #'font-size = #4
 		\\override Voice.Stem #'transparent = ##t
 		\\override Voice.Flag #'transparent = ##t
-		\\teeny }
+		\\teeny  \n }
     >>}"))
 
+(defun default-lily-paste()
+  ;; paste default string in new file
+  (interactive)
+  (insert "\\version \"2.18.0\"\n\\language \"deutsch\""))
+
+(defun keys-lily-hook ()
+  (local-set-key (kbd "<f5>") 'default-lily-paste))
+
+(add-hook 'LilyPond-mode-hook 'keys-lily-hook)
