3e3a2201789f1303dcd510d57a689ba5770d5ce7
krosenmann <krosenmann@gmail.com>
2017-09-10 14:45:06 +0300

    jjj

--- init.el
+++ init.el
@@ -31,40 +31,6 @@
 (add-hook 'text-mode-hook 'turn-on-auto-fill)
 
 
-;; List of additional LaTeX packages
-;(add-to-list 'org-export-latex-packages-alist '("" "cmap" t))
-;(add-to-list 'org-export-latex-packages-alist '("english,russian" "babel" t))
-
-;;Auto-complete
-
-;; (require 'auto-complete)
-;; (global-auto-complete-mode t)
-;; (require 'auto-complete-config)
-;; (ac-config-default)
-;; (eval-after-load "LilyPond-mode" (load-library "ac-lilypond"))
-
-;;webkit
-;; Don't need anymore
-;;(add-to-list 'load-path "~/kremacs/plugins/webkit")
-;;(add-to-list 'load-path "~/kremacs/plugins/emacs-epc")
-;;(add-to-list 'load-path "~/kremacs/plugins/emacs-deferred")
-;;(add-to-list 'load-path "~/kremacs/plugins/emacs-ctable")
-;;(require 'webkit)
-
-
-
-;; (eval-when-compile (require 'starters "~/kremacs/plugins/starters.el"))
-;; (require 'ox-cv)
-;; (add-to-list 'org-latex-classes
-;;              '("mymoderncv"
-;;                "\\documentclass\{moderncv\}
-;; \[NO-DEFAULT-PACKAGES\]
-;; \[NO-PACKAGES\]
-;; \[EXTRA\]"
-;; 	       ("\\section{%s}" . "\\section*{%s}")
-;;              ("\\subsection{%s}" . "\\subsection*{%s}")
-;;              ("\\subsubsection{%s}" . "\\subsubsection*{%s}"))) 
-;(add-to-list 'org-export-before-parsing-hook 'ox-cv-export-parse-employment)
 
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
