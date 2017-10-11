4c77132ba831d3e58b3ca3c09069bf56c674a466
Roman Zayrullin <krosenmann@gmail.com>
2017-10-11 16:52:14 +0300

    sldfjn

--- init-python.el
+++ init-python.el
@@ -36,7 +36,7 @@
   (insert "from IPython.core.debugger import Tracer; Tracer()()")
  )
 
-                                        ;TODO: Довести функцию до норм очистки
+                                        ;TODO: Довести функциlю до норм очистки
 (defun clear-bp()
   ;; Clear bp's in a buffer
   (interactive)
@@ -47,6 +47,10 @@
   "Extended keymaps for python-mode"
   (local-set-key (kbd "<f6>") 'python-bp))
 
+(defun virtual-env-focus-hook ()
+  "Hook for activate virtualenv on focus"
+      #'auto-virtualenvwrapper-activate)
+
 (add-hook 'python-mode-hook 'python-extend-keymap)
 (setq python-shell-interpreter "ipython"
       python-shell-interpreter-args "-i")
@@ -54,8 +58,8 @@
 
 (require 'auto-virtualenvwrapper)
 (add-hook 'python-mode-hook #'auto-virtualenvwrapper-activate)
-(add-hook 'focus-in-hook #'auto-virtualenvwrapper-activate)
-(add-hook 'window-configuration-change-hook #'auto-virtualenvwrapper-activate)
+(add-hook 'focus-in-hook 'virtual-env-focus-hook)
+(add-hook 'window-configuration-change-hook 'virtual-env-focus-hook)
             
 ;; Django
 (require 'pony-mode)
