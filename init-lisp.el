e1577bfd9db6ec4bad8dc871c95fa16ea67df87b
krosenmann <krosenmann@gmail.com>
2018-08-13 11:48:25 +0300

    Наконец-то нормальный сетап для лисподрочерства

--- init-lisp.el
+++ init-lisp.el
@@ -1,8 +1,15 @@
 ;; Guile and Scheme part
 
 (add-hook 'scheme-mode-hook 'eldoc-mode)
-
 ;; Elisp
 
 (add-hook 'emacs-lisp-mode-hook 'eldoc-mode)
 
+;; SBCL
+(load (expand-file-name "~/quicklisp/slime-helper.el"))
+(setq inferior-lisp-program "/usr/bin/sbcl")
+(require 'slime)
+(setq slime-contribs '(slime-fancy))
+(slime-setup)
+
+(add-hook 'lisp-mode-hook 'slime-mode)
