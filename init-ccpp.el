82e3bae8b315b4343d1c57b9f8c6d80d18d0e726
Roman Zayrullin <zayrullin.roman@220-volt.ru>
2018-09-09 17:44:32 +0300

    Удалить flymake

--- init-ccpp.el
+++
@@ -1,8 +0,0 @@
-(require 'flymake-cppcheck)
-(add-hook 'c++-mode-hook 'flymake-cppcheck-load)
-(add-hook 'c-mode-hook 'flymake-cppcheck-load)
-
-(custom-set-variables
- '(flymake-cppcheck-enable "warning,perfomance,information,style"))
-(custom-set-variables
- '(flymake-cppcheck-command "cppcheck"))
